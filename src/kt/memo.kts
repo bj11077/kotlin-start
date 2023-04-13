40a79fa70719aa49971be196d66ff2fcfe46ed01
bjp <bj11077@gmail.com>
2023-04-13 17:58:29 +0900

    2-6 RAW문자열 보는중

---
+++ src/kt/memo.kts
@@ -0,0 +1,11 @@
+val name = "Eve"
+
+val memo = """Dear $name, a quick reminder about the
+party we have scheduled next Tuesday at
+the 'Low Ceremony Cafe' at Moon. | Please plan to..."""
+println(memo)
+/**
+Dear Eve, a quick reminder about the
+party we have scheduled next Tuesday at
+the 'Low Ceremony Cafe' at Moon. | Please plan to...
+ */
