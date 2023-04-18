bc94fbe209ad97c147bc43ef4f868bf34e2d0ef1
bjp <bj11077@gmail.com>
2023-04-18 12:40:17 +0900

    2-7 표현식 / 명령문

---
+++ src/kt/expressionKotl.kts
@@ -0,0 +1,9 @@
+fun canVote(name:String, age:Int):String{
+    val status = if (age > 17) "yes, please vote" else "nope, please come back"
+    return "$name, $status"
+}
+println(canVote("Eve",12))
+
+/**
+ * Eve, nope, please come back
+ */
---
+++ src/kt/statementKotl.kts
@@ -0,0 +1,14 @@
+fun canVote(name:String, age:Int):String{
+    var status: String
+    if(age > 17){
+        status = "yes, please vote"
+    }else{
+        status = "nope, please come back"
+    }
+    return "$name, $status"
+}
+println(canVote("Eve",12))
+
+/**
+ * Eve, nope, please come back
+ */
